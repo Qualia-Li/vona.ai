f07c5f665736bd2888e84430aa60b416c6329572
Qualia-Li <Qualia-Li@users.noreply.github.com>
2025-04-29 13:33:05 -0700

    refactor: Update next.config.ts configurations and redirects

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"], // Add any image domains you'll use
+  },
 
   async redirects() {
-		return [
-			{
-				source: '/meet',
-				destination: 'https://calendly.com/quanlai/30min',
-				permanent: true,
-			},
+    return [
       {
-        source: '/demo',
-        destination: 'https://www.loom.com/share/bca84bcf13634344ae1e50d8acf19b71?sid=e8cea14f-ba95-45de-b909-5f8a4c358eda',
+        source: "/meet",
+        destination: "https://calendly.com/quanlai/30min",
         permanent: true,
-      }
-		];
+      },
+      {
+        source: "/demo",
+        destination:
+          "https://www.loom.com/share/bca84bcf13634344ae1e50d8acf19b71?sid=e8cea14f-ba95-45de-b909-5f8a4c358eda",
+        permanent: false,
+      },
+    ];
   },
 };
 
