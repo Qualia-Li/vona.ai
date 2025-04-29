9711ff7b8e0db3076d8962f09d4740f7536e4ac5
Qualia-Li <Qualia-Li@users.noreply.github.com>
2025-04-29 12:45:52 -0700

    feat: add redirects for /meet and /demo pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async redirects() {
+		return [
+			{
+				source: '/meet',
+				destination: 'https://calendly.com/quanlai/30min',
+				permanent: true,
+			},
+      {
+        source: '/demo',
+        destination: 'https://www.loom.com/share/bca84bcf13634344ae1e50d8acf19b71?sid=e8cea14f-ba95-45de-b909-5f8a4c358eda',
+        permanent: true,
+      }
+		];
+  },
 };
 
 export default nextConfig;
