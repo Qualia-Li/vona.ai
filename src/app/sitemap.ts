7e139e6372db5b5e73496ea915b8b493688b6cfb
Qualia-Li <Qualia-Li@users.noreply.github.com>
2025-05-20 22:54:50 -0700

    feat: Add new career page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,6 +20,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 0.9,
     },
+    {
+      url: 'https://enception.ai/career',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
     // Add more URLs based on your routes
   ];
 }
